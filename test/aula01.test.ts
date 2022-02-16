7154b82768833defcf648f7b2170c90754ee782f
Rui Valim <r.valim.junior@gmail.com>
2022-02-16 10:09:51 -0300

    Initial commit

---
+++ test/aula01.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Aula01 from '../lib/aula01-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aula01-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Aula01.Aula01Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
